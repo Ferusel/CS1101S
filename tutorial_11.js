43054a1a7f006b13ef72744e599acb9f1a8e2fd5
Source Academy User <No public email provided>
2021-10-24 17:10:16 +0800

    Changes made from Source Academy

--- tutorial_11.js
+++ tutorial_11.js
@@ -1,13 +1,21 @@
-// Increase each element in a given stream by a given factor
+// Scale each element in a given stream by a given factor
 function scale_stream(c, stream) {
     return stream_map(x => c * x, stream);
 }
 
-const A = pair(1, () => scale_stream(2, A));
-function mul_streams(a,b) {
-    return pair(head(a) * head(b),
-                () => mul_streams(stream_tail(a), stream_tail(b)));
-}
+// Essentially calculates 2^n where n is the depth of the stream traversed
+const A = pair(1, () => scale_stream(2, A)); // Call stream_tail() on A
+// const A = pair(1, scale_stream(2, A));
+// const A = pair(1, stream_map(x => 2*x, A));
+// const A = pair(1, pair(2, () => stream_map(x => 2*x, stream_tail(A))));
+// const A = pair(1, pair(2, () => stream_map(x => 2*x, pair(2, () => scale_stream(A))))); // Call stream_tail()
+// const A = pair(1, pair(2, stream_map(x => 2*x, pair(2, () => scale_stream(A)))));
+// const A = pair(1, pair(2, pair(4, () => stream_map(x => 2*x, stream_tail(A)))));
 
-const integers = integers_from(1);
-const B = pair(1, () => mul_streams(B, integers));
+// function mul_streams(a,b) {
+//     return pair(head(a) * head(b),
+//                 () => mul_streams(stream_tail(a), stream_tail(b)));
+// }
+
+// const integers = integers_from(1);
+// const B = pair(1, () => mul_streams(B, integers));
