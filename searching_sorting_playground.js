be5f0efc45d7932cd42161b351a6560a035625ae
Source Academy User <No public email provided>
2021-11-14 18:17:52 +0800

    Changes made from Source Academy

--- searching_sorting_playground.js
+++ searching_sorting_playground.js
@@ -33,5 +33,37 @@ function binary_search_iter(A, v) {
     }
     return (low <= high);
 }
-binary_search([1,2,3,4,5,6,7,8,9], 8);
-binary_search_iter([1,2,3,4,5,6,7,8,9], 8);
+
+// binary_search([1,2,3,4,5,6,7,8,9], 8);
+// binary_search_iter([1,2,3,4,5,6,7,8,9], 8);
+
+
+// Selection Sort
+function selection_sort(A) {
+    const len = array_length(A);
+
+    for (let i = 0; i < len - 1; i = i + 1) {
+        let min_pos = find_min_pos(A, i, len - 1);
+        swap(A, i, min_pos);
+    }
+}
+
+function find_min_pos(A, low, high) {
+    let min_pos = low;
+    for (let j = low + 1; j <= high; j = j + 1) {
+        if (A[j] < A[min_pos]) {
+            min_pos = j;
+        }
+    }
+    return min_pos;
+}
+
+function swap(A, x, y) {
+    const temp = A[x];
+    A[x] = A[y];
+    A[y] = temp;
+}
+
+// const A = [3, 9, 2, 1, 6, 5, 3, 8];
+// selection_sort(A);
+// A;
