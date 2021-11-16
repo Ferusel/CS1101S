b99096b85a45c4b015985f853d9b37bdca7e5064
Source Academy User <No public email provided>
2021-11-17 02:19:33 +0800

    Changes made from Source Academy

---
+++ CS1101S_2014_Finals.js
@@ -0,0 +1,54 @@
+// let x = list(pair(1, tail(x)), list(3, 4));
+// draw_data(x);
+
+let x = 1;
+function foo(a) {
+    let x = a*2;
+    return gee(x);
+}
+function gee(b) {
+    x = x+2;
+    return b+x;
+}
+// foo(3);
+function mergeB(xs, ys) {
+    if (is_null(xs)) {
+        return ys;
+    } else if (is_null(ys)) {
+        return xs;
+    } else {
+        display(xs);
+        display(ys);
+        display("====");
+        let hx = head(xs);
+        let hy = head(ys);
+        if (hx < hy) {
+            let pointer = tail(xs);
+            set_tail(xs, ys);
+            mergeB(pointer, tail(ys));
+            return xs;
+        } else if (hy <= hx) {
+            let pointer = tail(ys);
+            set_tail(ys, xs);
+            mergeB(tail(xs), pointer);
+            return ys;
+        }
+    }
+}
+
+let lst1 = list(2, 3, 6);
+let lst2 = list(1, 4, 5);
+// mergeB(lst1, lst2);
+
+function powerset(set){
+    if (is_null(tail(set))) {
+        return list(list(head(set)), list());
+    } else {
+        let res = powerset(tail(set));
+        let curr = head(set);
+        return append(res, map(x => append(x, list(curr)), res));
+    }
+}
+
+let set1 = list(1, 2, 3, 4);
+display_list(powerset(set1));
